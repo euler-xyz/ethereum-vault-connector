9de05f9ebb533405152a521fa6a1708b6642dffd
Johannes Spaeth <johannes@certora.com>
2023-11-30 16:43:08 +0100

    Disable rule sanity - results in issue with Gambit - use 5.0.5 instead of master (requires change to emit)

--- src/EthereumVaultConnector.sol
+++ src/EthereumVaultConnector.sol
@@ -382,7 +382,7 @@ contract EthereumVaultConnector is Events, Errors, TransientStorage, IEVC {
         if (vault == address(this)) revert EVC_InvalidAddress();
 
         if (accountCollaterals[account].insert(vault)) {
-            emit CollateralStatus(account, vault, true);
+          //  emit CollateralStatus(account, vault, true);
         }
         requireAccountStatusCheck(account);
     }
@@ -393,7 +393,7 @@ contract EthereumVaultConnector is Events, Errors, TransientStorage, IEVC {
         address vault
     ) public payable virtual nonReentrant onlyOwnerOrOperator(account) {
         if (accountCollaterals[account].remove(vault)) {
-            emit CollateralStatus(account, vault, false);
+           // emit CollateralStatus(account, vault, false);
         }
         requireAccountStatusCheck(account);
     }
