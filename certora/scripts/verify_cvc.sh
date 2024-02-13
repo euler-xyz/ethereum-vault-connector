8f62d96a56e85b19fd8edcde2f7b3c3369d651c8
Andrew Ferraiuolo <andrew@certora.com>
2024-02-13 10:18:08 +0000

    Delete old files

--- certora/scripts/verify_cvc.sh
+++
@@ -1,6 +0,0 @@
-if [[ "$1" ]]
-then
-    RULE="--rule $1"
-fi
-
-certoraRun ./certora/conf/cvc.conf $RULE
--- certora/scripts/verify_cvc_nonReentrant.sh
+++
@@ -1,6 +0,0 @@
-if [[ "$1" ]]
-then
-    RULE="--rule $1"
-fi
-
-certoraRun ./certora/conf/cvc_nonReentrant.conf $RULE
