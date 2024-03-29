ffd67eb6d022d6fb3e3a35a312abb839f6d7c639
kasperpawlowski <25374117+kasperpawlowski@users.noreply.github.com>
2024-03-29 17:02:24 +0100

    fix: audit Omniscia EVR-06M

--- src/interfaces/IVault.sol
+++ src/interfaces/IVault.sol
@@ -15,12 +15,16 @@ interface IVault {
     function disableController() external;
 
     /// @notice Checks the status of an account.
+    /// @dev This function must only revert if the account status is invalid. If this function reverts due to any other
+    /// reason, it may render the account unusable with possibly no way to recover funds.
     /// @param account The address of the account to be checked.
     /// @return magicValue Must return the bytes4 magic value 0xb168c58f (which is a selector of this function) when
     /// account status is valid, or revert otherwise.
     function checkAccountStatus(address account, address[] calldata collaterals) external returns (bytes4 magicValue);
 
     /// @notice Checks the status of the vault.
+    /// @dev This function must only revert if the vault status is invalid. If this function reverts due to any other
+    /// reason, it may render the account unusable with possibly no way to recover funds.
     /// @return magicValue Must return the bytes4 magic value 0x4b3d1223 (which is a selector of this function) when
     /// account status is valid, or revert otherwise.
     function checkVaultStatus() external returns (bytes4 magicValue);
